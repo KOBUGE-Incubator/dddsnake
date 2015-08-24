54110b04351565b6f43c72d531ba953bc349a2a5
Paul Von Zimmermam <pcvonz@gmail.com>
2015-08-23 22:40:19 -0700

    tweaked the controls, now all you do is press left and right to move and up and down to change height. A lot easier with camera

--- snake.gd
+++ snake.gd
@@ -12,22 +12,23 @@ var go_left
 var go_up
 var go_down
 var go_backward
-var go_forward
+
 var speed = .2
 var tail
 var d = 0
 var x_axis_count = 0
-
-
-func _fixed_process(delta):
+var up_axis_count = 0
+var down_axis_count = 0
+var go_forward = true
+func _input(ev):
 	set_rotation(Vector3(.05, 0, 0))
-	if (Input.is_action_pressed(("ui_right"))):
+	if (Input.is_action_pressed("ui_right")):
 		go_right = true
 		go_left = false
 		go_up = false
 		go_down = false
 		go_forward = false
-		go_backward = false
+		x_axis_count += 1
 
 	if (Input.is_action_pressed("ui_left")):
 		go_right = false
@@ -35,61 +36,80 @@ func _fixed_process(delta):
 		go_up = false
 		go_down = false
 		go_forward = false
-		go_backward = false
-	if (Input.is_action_pressed("ui_up")):
-		go_right = false
-		go_left = false
-		go_up = true
-		go_down = false
-		go_forward = false
-		go_backward = false
+		x_axis_count += -1
 	if (Input.is_action_pressed("ui_down")):
-		go_right = false
-		go_left = false
 		go_up = false
 		go_down = true
-		go_forward = false
-		go_backward = false
-	if (Input.is_action_pressed("ui_forward")):
-		go_right = false
-		go_left = false
-		go_up = false
-		go_down = false
-		go_forward = true
-		go_backward = false
-	if (Input.is_action_pressed("ui_backward")):
-		go_right = false
-		go_left = false
-		go_up = false
-		go_down = false
-		go_forward = false
-		go_backward = true
 
+		up_axis_count = 0
+		down_axis_count += 1
+	if (Input.is_action_pressed("ui_up")):
+		go_up = true
+		go_down = false
 
-	if(go_right == true):
-		set_rotation(Vector3(0, -1.571, 0))
-		variable_z = speed
-		move_snake(variable_z, 0, 0)
-	if(go_left == true):
-		variable_z = -speed
-		set_rotation(Vector3(0, 1.571, 0))
-		move_snake(variable_z, 0, 0)
-	if (go_up == true):
+		up_axis_count += 1
+		down_axis_count = 0
+		
+func _fixed_process(delta):
+
+	
+	if(!(go_up or go_down)):
+		if(go_right == true):
+			if(x_axis_count == 1):
+				set_rotation(Vector3(0, -1.571, 0))
+				variable_z = speed
+				move_snake(variable_z, 0, 0)
+			elif(x_axis_count == 2):
+				variable_x = speed
+				set_rotation(Vector3(0, 3.142, 0))
+				move_snake(0, 0, variable_x)
+			elif(x_axis_count == 3):
+				variable_z = -speed
+				set_rotation(Vector3(0, 1.571, 0))
+				move_snake(variable_z, 0, 0)
+			elif(x_axis_count == 4):
+				variable_x = -speed
+				move_snake(0, 0, variable_x)
+			else:
+				x_axis_count = 1
+			
+		elif(go_left == true):
+			if(x_axis_count == 1):
+				set_rotation(Vector3(0, -1.571, 0))
+				variable_z = speed
+				move_snake(variable_z, 0, 0)
+			elif(x_axis_count == 2):
+				variable_x = speed
+				set_rotation(Vector3(0, 3.142, 0))
+				move_snake(0, 0, variable_x)
+			elif(x_axis_count == 3):
+				variable_z = -speed
+				set_rotation(Vector3(0, 1.571, 0))
+				move_snake(variable_z, 0, 0)
+			elif(x_axis_count == 4):
+				variable_x = -speed
+				move_snake(0, 0, variable_x)
+			else:
+				x_axis_count = 4
+	elif (go_up == true):
 		variable_y = speed
 		set_rotation(Vector3(1.571, 0, 0))
 		move_snake(0, variable_y, 0)
-	if(go_down == true):
+		if(up_axis_count > 1):
+			go_up = false
+			up_axis_count = 0 
+
+	elif(go_down == true):
 		variable_y = -speed
 		set_rotation(Vector3(-1.571, 0, 0))
 		move_snake(0, variable_y, 0)
-	if(go_forward == true):
+		if(down_axis_count > 1):
+			go_down = false
+			down_axis_count = 0
+	if((go_forward == true) and !(go_down or go_up)):
 		variable_x = -speed
-		set_rotation(Vector3(0, 0, 0))
 		move_snake(0, 0, variable_x)
-	if(go_backward == true):
-		variable_x = speed
-		set_rotation(Vector3(0, 3.142, 0))
-		move_snake(0, 0, variable_x)
+
 
 
 func move_snake(variable_z, variable_y, variable_x):
@@ -100,4 +120,5 @@ func _ready():
 	# Initialization here
 	set_collide_with_static_bodies(true)
 	set_fixed_process(true)
+	set_process_input(true)
 
