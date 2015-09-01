6ade2f22d226497e9c2939898342de20ee0a17f9
Paul Von Zimmermam <pcvonz@gmail.com>
2015-08-31 19:24:08 -0700

    Separated the controls back into input

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -2,46 +2,49 @@ extends RigidBody
 
 # controls
 var delay_start = .42
-var speed = 100
+var speed = 500
 var force = Vector3(0,0,-1)
 # Set at the beginning in order to make the snake automatically start moving forward
 var array = []
 var right
-var right_prev = false
 var left
-var left_prev = false
 var up
-var up_prev = false
 var down
-var down_prev = false
 
+func _input(event):
+	if(Input.is_action_pressed("ui_right")):
+		right = true
+	if(Input.is_action_pressed("ui_left")):
+		left = true
+	if(Input.is_action_pressed("ui_down")):
+		down = true
+	if(Input.is_action_pressed("ui_up")):
+			up = true
 func _fixed_process(delta):
-	right = Input.is_action_pressed("ui_right")
-	if (right and not right_prev):
+
+	if (right):
 		rotate_y(PI/2)
 		force = force.rotated(Vector3(0,1,0), PI/2)
-	right_prev = right
+		right = false
 	
-	left = Input.is_action_pressed("ui_left")
-	if (left and not left_prev):
+	if (left):
 		rotate_y(-PI/2)
 		force = force.rotated(Vector3(0,1,0), -PI/2)
-	left_prev = left
+		left = false
 	
-	up = Input.is_action_pressed("ui_up")
-	if (up and not up_prev):
+	if (up):
 		rotate_x(PI/2)
 		force = force.rotated(Vector3(1,0,0), PI/2)
-	up_prev = up
+		up = false
 	
-	down = Input.is_action_pressed("ui_down")
-	if (down and not down_prev):
+	if (down):
 		rotate_x(-PI/2)
 		force = force.rotated(Vector3(1,0,0), -PI/2)
-	down_prev = down
+		down = false
 	
 	set_linear_velocity(force*speed*delta)
 	
 func _ready():
+	set_process_input(true)
 	set_fixed_process(true)
 
